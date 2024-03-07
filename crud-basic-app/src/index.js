79fd91b5146b011b26f57aef08fcd07f467e2332
rupesh989 <rupeshbavaskar24@gmail.com>
2024-03-07 21:39:08 +0530

    Added commits

--- crud-basic-app/src/index.js
+++ crud-basic-app/src/index.js
@@ -2,10 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 
+import { configureStore } from "@reduxjs/toolkit";
+import UserReducer from "./UserReducer";
+
+const store = configureStore({
+  reducer: {
+    users: UserReducer,
+  },
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
