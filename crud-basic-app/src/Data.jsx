093f5f6f3fb3b03048869985a297a40bb88d3c96
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-08 19:55:17 +0530

    Update Data.jsx

--- crud-basic-app/src/Data.jsx
+++ crud-basic-app/src/Data.jsx
@@ -1,1 +1,17 @@
+export const userList = [
+    {
+        name: "Rohit Sharma",
+        email: "rohit45@gmail.com",
+        id: 1
+    }, {
+        name: "Virat Kohli",
+        email: "virat18@gmail.com",
+        id: 2
 
+    },
+    {
+        name: "Shubhman gill",
+        email: "shubhman77@gmail.com",
+        id: 3
+    }
+]
