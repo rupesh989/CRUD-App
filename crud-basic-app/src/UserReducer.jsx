6b2e112c4a3921a26333015be6be58a2131b4ef2
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-10 12:28:39 +0530

    Update UserReducer.jsx

--- crud-basic-app/src/UserReducer.jsx
+++ crud-basic-app/src/UserReducer.jsx
@@ -1,1 +1,20 @@
 
+import { createSlice } from "@reduxjs/toolkit";
+import { userList } from "./Data";
+
+
+const userSlice = createSlice({
+    name: "users",
+    initialState: userList,
+    reducers: {
+        addUser: (state, action) => {
+            console.log(action)
+            state.push(action.payload)
+        },
+       
+        }
+    }
+})
+
+export const { addUser, updateUser, deleteUser } = userSlice.actions
+export default userSlice.reducer;
