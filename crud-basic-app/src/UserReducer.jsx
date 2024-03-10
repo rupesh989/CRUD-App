1a4f54fb98de471585cea448831d69b2120fe35e
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-10 12:29:34 +0530

    Update UserReducer.jsx

--- crud-basic-app/src/UserReducer.jsx
+++ crud-basic-app/src/UserReducer.jsx
@@ -11,7 +11,22 @@ const userSlice = createSlice({
             console.log(action)
             state.push(action.payload)
         },
-       
+        updateUser: (state, action) => {
+            const { id, name, email } = action.payload
+            const uu = state.find(user => user.id == id)
+            if (uu) {
+                uu.name = name;
+                uu.email = email
+            }
+
+        },
+        deleteUser: (state, action) => {
+            const { id } = action.payload
+            const uu = state.find(user => user.id == id)
+            if (uu) {
+                return state.filter(f => f.id !== id)
+            }
+
         }
     }
 })
